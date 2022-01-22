c1d882d2e53daf91a2fb05d480fbeeee7b5aff5e
UNKNOWN8884 <97676159+UNKNOWN8884@users.noreply.github.com>
2022-01-22 20:10:44 +0530

    Update tagall.py

--- MashaRoBot/modules/tagall.py
+++ MashaRoBot/modules/tagall.py
@@ -34,7 +34,7 @@ async def _(event):
     await event.delete()
 
 
-__mod_name__ = "ᴛᴀɢᴀʟʟ💫"
+__mod_name__ = "ͲᎪᏀ-ᎪᏞᏞ🎐"
 __help__ = """
-- /tagall : Tag everyone in a chat
+- /tagall : Tᴀɢ ᴇᴠᴇʀʏᴏɴᴇ ɪɴ ᴀ ᴄʜᴀᴛ
 """
